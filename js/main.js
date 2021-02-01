02ab8e74bac58b8d9e57e446f4dfa54e275c2f24
ana <ana.speakproject@gmail.com>
2021-02-01 10:04:07 -0600

    Поправила синтаксис функций

--- js/main.js
+++ js/main.js
@@ -1,33 +1,23 @@
-/* No.1 Функция, возвращающая случайное целое число из переданного диапазона включительно (Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
-let getRandomIntInclusive = function (min, max) {
-  if (max > min && min >= 0) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+// No.1 Функция, возвращающая случайное целое число из переданного диапазона включительно (Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+const getRandomInteger = function (min, max) {
+  if (min < 0) {
+    throw new Error('Отрицательные значения запрещены');
   }
 
-  if (max <= min || min < 0) {
-    alert('Not gonna work');
+  if (min > max) {
+    throw new Error('Значение "от" не может быть больше, чем значение "до"');
   }
-}
-getRandomIntInclusive(0, 10); */
+
+  const minInt = Math.ceil(min);
+  const maxInt = Math.floor(max);
+  return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
+};
+
+getRandomInteger(0, 10);
 
 
-// No. 2 Функция как сверху, но с тернарными операторами
-let getRandomIntInclusive = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  return (max > min && min >= 0) ? Math.floor(Math.random() * (max - min + 1)) + min :
-    (max <= min || min < 0) ? 'Я не работаю в отрицательном диапазоне' :
-    // Здесь я плохо понимаю, почему нужна "последняя строка" без условия – иначе не работает.
-      null;
-}
-getRandomIntInclusive(0, 10);
-
-
-// No. 3 Функция для проверки максимальной длины строки
-let getLineLength = function (line, maxLength) {
+// No. 2 Функция для проверки максимальной длины строки
+const getLineLength = function (line, maxLength) {
   return (line.length <= maxLength) ? true : false;
 };
 
