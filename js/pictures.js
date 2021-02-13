f2633f8eafcb942cf1060d125d8a32faea555a43
ana <ana.speakproject@gmail.com>
2021-02-12 20:11:32 -0600

    Отрисуй меня полностью (часть 1)

---
+++ js/pictures.js
@@ -0,0 +1,18 @@
+import {similarPhotoDescriptions} from './util.js';
+
+const otherUsersPictures = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const userPictures = similarPhotoDescriptions;
+
+const picturesBlock = document.createDocumentFragment();
+
+userPictures.forEach(({url, comments, likes}) => {
+  const otherUserPicture = pictureTemplate.cloneNode(true);
+  otherUserPicture.querySelector('.picture__img').src = url;
+  otherUserPicture.querySelector('.picture__comments').textContent = comments;
+  otherUserPicture.querySelector('.picture__likes').textContent = likes;
+  picturesBlock.appendChild(otherUserPicture);
+});
+
+otherUsersPictures.appendChild(picturesBlock);
